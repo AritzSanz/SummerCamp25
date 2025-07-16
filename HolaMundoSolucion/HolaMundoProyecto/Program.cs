6c7c57c90a022b14bef61935d592d972b59a44d4
AritzSanz <aritz.s@hotmail.es>
2025-07-16 11:36:16 +0200

    Primer Hola Mundo

---
+++ HolaMundoSolucion/HolaMundoProyecto/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("============ ");
+
+        
+        Console.WriteLine(@"                                 ,'', ");
+        Console.WriteLine(@"                              .a@a. ");
+        Console.WriteLine(@"                              `@@@' ");
+        Console.WriteLine(@"                         .,:::::::::::,. ");
+        Console.WriteLine(@"                     .,%%::::%%:::%:::%%%%,. ");
+        Console.WriteLine(@"                   .%%%%%%::%%%%:%%%:::%%%%%%. ");
+        Console.WriteLine(@"              .a@@@@@@@@a%%:%%%%%%%%::%%%%%%%% ");
+        Console.WriteLine(@"  .%%%,.     a@;@@@;@@@;@@;;%%%%%%%%%:mm:::::::::mm,.    .,%%%, ");
+        Console.WriteLine(@" %%%%%%%%,;;;@;;;@@;;@@;;;@;;;%%%%mmmmm::mm:::mm:::mmmm,%%%%%%%% ");
+        Console.WriteLine(@"`%%%%%%%%%%;;;;;;@;;;;@;;;;;;;;;mmmmmm::mmmm::mmmm::mm%%%%%%%%%%' ");
+        Console.WriteLine(@" `%%%%%%%%%%%%:::@::::::::::::::mmmmmmm:mmmmmmmmmmm:%%%%%%%%%%%' ");
+        Console.WriteLine(@".\\\\,\\\\,\\\\,\\\\,\\\\,\\\\,\|/,////,////,////,/::/,////,////. ");
+        Console.WriteLine(@" \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\|//////////////////://////////// ");
+        Console.WriteLine(@"   `\\\\\\\\\\\\\\\\\\\\\\\\\\\\|/////////////////://////////' ");
+        Console.WriteLine(@"        `\\\\\\\\\\\\\\\\\\\\\\\|/////////////////://///' ");
+        Console.WriteLine(@"             `\\\\\\\\\\\\\\\\\\|///////////////////' ");
+        Console.WriteLine(@"                   `\\\\\\\\\\\\|////////////' ");
+        Console.WriteLine(@"                          `\\\\\|/////'          ;"); 
+        Console.WriteLine(@"                            `\\\|///'           .:. ");
+        Console.WriteLine(@"                              \\|//            ::::: ");
+        Console.WriteLine(@"                           .,///|\\\,.         `:::' ");
+        Console.WriteLine(@"                        .///////|\\\\\\\.             ");
+            }
+        }
